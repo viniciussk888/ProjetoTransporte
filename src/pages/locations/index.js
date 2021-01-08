806b2bf3710bc99664c9259f0302180b5c9c6a54
vinicius <vinicius.cross07@gmail.com>
2021-01-07 23:22:21 -0300

    headermap

--- src/pages/locations/index.js
+++ src/pages/locations/index.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import MapView from "react-native-maps";
-import {
-  requestPermissionsAsync,
-  getCurrentPositionAsync,
-} from "expo-location";
+import { requestPermissionsAsync, getCurrentPositionAsync, } from "expo-location";
 
 import styles from "./styles";
 
@@ -13,14 +10,14 @@ function Locations() {
 
   useEffect(() => {
     async function loadInitialPosition() {
-      const { granted } = await requestPermissionsAsync();
+      const {granted} = await requestPermissionsAsync();
 
       if (granted) {
-        const { coords } = await getCurrentPositionAsync({
+        const {coords} = await getCurrentPositionAsync({
           enableHighAccuracy: true,
         });
 
-        const { latitude, longitude } = coords;
+        const {latitude, longitude} = coords;
 
         setCurrentRegion({
           latitude,
@@ -36,10 +33,20 @@ function Locations() {
   }, []);
 
   return (
+    <>
     <View style={styles.container}>
+    <View style={styles.header}>
+      <View style={styles.contentTitle}>
+      <Text style={styles.textTitle}>Encontre aqui locais importantes.</Text>
+      </View>
+        
+        <Text style={styles.textCategory}>Categorias</Text>
+
+      </View>
       <MapView initialRegion={currentRegion} style={styles.map} />
     </View>
-  );
+    </>
+    );
 }
 
 export default Locations;
--- src/pages/locations/styles.js
+++ src/pages/locations/styles.js
@@ -9,7 +9,32 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    minHeight: "75%",
+  },
+  header: {
+    flex: 1,
+    minHeight: '25%',
+    width: Dimensions.get("window").width,
+    padding: 30,
+    backgroundColor: "#afb7bd",
+    flexDirection: 'column',
+    alignItems: "flex-start",
+  },
+  contentTitle: {
+    alignSelf: 'center',
+  },
+  textTitle: {
+    fontFamily: "Archivo_700Bold",
+    color: "#000",
+    fontSize: 16,
+    lineHeight: 25,
+  },
+  textCategory: {
+    marginTop: 10,
+    fontFamily: "Archivo_400Regular",
+    color: "#000",
+    fontSize: 14,
+    lineHeight: 25,
   },
 });
 
